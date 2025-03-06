5a06588e5f34af158d705e7b6ad7d202d02e3551
Chris-gus <chrisgus2605@gmail.com>
2025-03-06 10:40:28 -0300

    Feita atividade de P.O.O

---
+++ ConsoleApp5/Pedido.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Pedido
+    {
+        public int Numero;
+        public List<Produto> Produtos = new List<Produto>();
+
+        public double totalPedido()
+        {
+            double total = 0;
+            foreach (Produto produto in Produtos)
+            {
+                total += produto.Preco;
+            }
+            return total;
+        }
+        public void ExibirPedido()
+        {
+            Console.WriteLine($"Pedido nº {Numero}");
+            Console.WriteLine("Itens: ");
+            foreach (Produto produto in Produtos)
+            {
+                Console.WriteLine($"Produto: {produto.Nome}");
+                Console.WriteLine($"Preço: R${produto.Preco}");
+                Console.WriteLine();
+            }
+            Console.WriteLine($"total: R${totalPedido()}\n");
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pedido> pedidos = new List<Pedido>();
+            Produto hamburguer = new Produto();
+            hamburguer.Nome = "Hambúrguer";
+            hamburguer.Preco = 25.00;
+            Produto batataFrita = new Produto();
+            batataFrita.Nome = "Batata-Frita";
+            batataFrita.Preco = 8.00;
+            Produto refrigerante = new Produto();
+            refrigerante.Nome = "Refrigerante";
+            refrigerante.Preco = 6.50;
+
+            Pedido pedido1 = new Pedido();
+            pedido1.Numero = 1;
+            pedido1.Produtos = new List<Produto>();
+            pedido1.Produtos.Add(hamburguer);
+            pedido1.Produtos.Add(batataFrita);
+            pedidos.Add(pedido1);
+
+            Pedido pedido2 = new Pedido();
+            pedido2.Numero = 2;
+            pedido2.Produtos = new List<Produto>();
+            pedido2.Produtos.Add(refrigerante);
+            pedido2.Produtos.Add(hamburguer);
+            pedidos.Add(pedido2);
+
+            foreach (Pedido pedido in pedidos)
+            {
+                pedido.ExibirPedido();
+            }
+
+
+        }
+
+    }
+}
